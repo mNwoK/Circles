457ea4afb92995b8977e8b914c5a39a262de51c0
mNwoK <man.nikeman.21@gmail.com>
2022-11-24 21:15:58 +0300

    ы

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-from PyQt5 import uic
+import random
 import sys
 
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5 import uic
+from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
-import random
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi("main.ui", self)
+        uic.loadUi("Ui.ui", self)
         self.pushButton.clicked.connect(self.paint)
         self.do_paint = False
 
-    def sharik(self):
+    def paintEvent(self, event):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
-            a = random.randint(0, 100)
-            qp.drawEllipse(QPoint(0, 0), a, a)
+            self.sharik(qp)
             qp.end()
+
+    def sharik(self, qp):
+        if self.do_paint:
+            qp.setBrush(QColor(255, 255, 0))
+            a = random.randint(0, 200)
+            qp.drawEllipse(QPoint(400, 300), a, a)
 
     def paint(self):
         self.do_paint = True
         self.repaint()
 
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec())
